f97f1e5ef6b70cc83289d5e531917a9659473f7f
bogdandacic <bogdan.dacic@pm.me>
2026-06-10 23:10:33 +0300

    Prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"],
-  });
+export const prisma = new PrismaClient({
+  accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,  // prisma://...
+  log: ["error", "warn"],
+});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
